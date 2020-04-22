0dccafbb60618e8cdbf47c3ddc589ef0476a038b
Facundo HM <facundo.hmz@gmail.com>
2020-04-21 23:59:13 -0300

    Implement env file

--- src/virtual-device.py
+++ src/virtual-device.py
@@ -1,9 +1,16 @@
 from click import command, option, echo
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# test env var
+print(os.getenv("GCP_PROJECT_ID"))
 
 @command()
 @option('--num_messages', default=1, help='Number of messages.')
 @option('--device_messages', prompt='Your device message', help='The message to send.')
-def send_message(num_messages, device_messages):
+def send_message(num_messages: int, device_messages: str):
     """Simple program that sends messages."""
     for _ in range(num_messages):
         echo('Sending message: %s' % device_messages)
